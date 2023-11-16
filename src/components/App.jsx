02c6ea9838750e32d58a0799aabad19b292dae59
Léon Kiefer <leon.kiefer@proton.me>
2023-11-16 11:23:02 +0100

    Import dashboard

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
+import { darkTheme } from '../theme/Dark';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import Dashboard from './Dashboard';
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
 
