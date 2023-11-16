2f9a6e769d92eeb72b5bac8ff81d23b7f8329605
Léon Kiefer <leon.kiefer@proton.me>
2023-11-16 14:30:21 +0100

    Update light theme import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import { lightTheme } from '../theme/Light';
+// import { lightTheme } from '../theme/Light';
+import { darkTheme } from '../theme/Dark';
 import { ThemeProvider } from '@mui/material';
 import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
       <Dashboard />
     </ThemeProvider>
   );
