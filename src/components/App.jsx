19050bf526525f4f12967bac90e29948630efd18
Léon Kiefer <leon.kiefer@proton.me>
2023-11-17 11:19:32 +0100

    Add comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@mui/material';
 import Dashboard from './Dashboard';
-import { darkTheme } from '../theme/Dark';
-// import { lightTheme } from '../theme/Light';
+import { darkTheme } from '../theme/DarkTheme';
+import { lightTheme } from '../theme/LightTheme';
 
 function App() {
   return (
