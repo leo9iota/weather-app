3e883a4a56ecdb4296004a2f7ae3b3bb76ea7692
Léon Kiefer <leon.kiefer@proton.me>
2023-11-22 09:57:43 +0100

    Add date/time formatting functions

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -2,7 +2,29 @@
 import React from 'react';
 import { Grid, Typography } from '@mui/material';
 
-function WeatherDisplay({ weather, formatDate, formatTime, isDayTime }) {
+function WeatherDisplay({ weather }) {
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  };
+
+  const formatTime = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  };
+
+  const isDayTime = (sunrise, sunset, currentTime) => {
+    return currentTime >= sunrise && currentTime < sunset ? 'Day' : 'Evening';
+  };
+
   return (
     <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
       <Grid item xs={6}>
