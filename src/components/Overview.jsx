566f68625009e01763185101f0a2587398e0aa0a
Léon Kiefer <leon.kiefer@proton.me>
2023-11-22 10:51:47 +0100

    Update icon with text alignment

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Paper, Typography, Grid } from '@mui/material';
+import ThermostatIcon from '@mui/icons-material/Thermostat'; // For temperature
+import OpacityIcon from '@mui/icons-material/Opacity'; // For humidity
+import AirIcon from '@mui/icons-material/Air'; // For wind speed
+import Brightness5Icon from '@mui/icons-material/Brightness5'; // For sunrise
+import Brightness4Icon from '@mui/icons-material/Brightness4'; // For sunset
 
 function Overview({ weatherData }) {
   // Function to format the timestamp into a readable time
@@ -8,30 +13,81 @@ function Overview({ weatherData }) {
     return date.toLocaleTimeString('en-US', {
       hour: '2-digit',
       minute: '2-digit',
-      hour12: true, // or false if you want 24-hour time
+      hour12: true,
     });
   };
 
   return (
-    <Paper elevation={3} sx={{ height: '60%', padding: 2 }}>
-      <Typography variant='h6' component='h2'>
+    <Paper elevation={3} sx={{ padding: 2, color: 'white' }}>
+      <Typography variant='h6' component='h2' sx={{ marginBottom: 2 }}>
         {weatherData?.name} Overview
       </Typography>
       {weatherData && (
         <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant='body1'>
-              Weather Description: {weatherData.weather[0].main} ({weatherData.weather[0].description})
-            </Typography>
-            <Typography variant='body1'>Feels Like: {weatherData.main.feels_like}°C</Typography>
-            <Typography variant='body1'>Cloudiness: {weatherData.clouds.all}%</Typography>
-            <Typography variant='body1'>Pressure: {weatherData.main.pressure} hPa</Typography>
-            <Typography variant='body1'>Humidity: {weatherData.main.humidity}%</Typography>
-            <Typography variant='body1'>Visibility: {weatherData.visibility / 1000} km</Typography>
-            <Typography variant='body1'>Wind Speed: {weatherData.wind.speed} m/s</Typography>
-            <Typography variant='body1'>Wind Direction: {weatherData.wind.deg}°</Typography>
-            <Typography variant='body1'>Sunrise: {formatTime(weatherData.sys.sunrise)}</Typography>
-            <Typography variant='body1'>Sunset: {formatTime(weatherData.sys.sunset)}</Typography>
+          <Grid item xs={12} container>
+            <Grid item xs={6} container alignItems="center">
+              <ThermostatIcon sx={{ color: 'white' }} />
+              <Typography variant='body1' sx={{ ml: 1 }}>
+                Feels Like
+              </Typography>
+            </Grid>
+            <Grid item xs={6} textAlign="right">
+              <Typography variant='body1'>
+                {weatherData.main.feels_like}°C
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} container>
+            <Grid item xs={6} container alignItems="center">
+              <OpacityIcon sx={{ color: 'white' }} />
+              <Typography variant='body1' sx={{ ml: 1 }}>
+                Humidity
+              </Typography>
+            </Grid>
+            <Grid item xs={6} textAlign="right">
+              <Typography variant='body1'>
+                {weatherData.main.humidity}%
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} container>
+            <Grid item xs={6} container alignItems="center">
+              <AirIcon sx={{ color: 'white' }} />
+              <Typography variant='body1' sx={{ ml: 1 }}>
+                Wind Speed
+              </Typography>
+            </Grid>
+            <Grid item xs={6} textAlign="right">
+              <Typography variant='body1'>
+                {weatherData.wind.speed} m/s
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} container>
+            <Grid item xs={6} container alignItems="center">
+              <Brightness5Icon sx={{ color: 'white' }} />
+              <Typography variant='body1' sx={{ ml: 1 }}>
+                Sunrise
+              </Typography>
+            </Grid>
+            <Grid item xs={6} textAlign="right">
+              <Typography variant='body1'>
+                {formatTime(weatherData.sys.sunrise)}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} container>
+            <Grid item xs={6} container alignItems="center">
+              <Brightness4Icon sx={{ color: 'white' }} />
+              <Typography variant='body1' sx={{ ml: 1 }}>
+                Sunset
+              </Typography>
+            </Grid>
+            <Grid item xs={6} textAlign="right">
+              <Typography variant='body1'>
+                {formatTime(weatherData.sys.sunset)}
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
       )}
