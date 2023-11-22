43984feb63710ebfe6b5b388e03df9567b5d2771
Léon Kiefer <leon.kiefer@proton.me>
2023-11-22 10:34:20 +0100

    Update overview component

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,15 +1,40 @@
-// Overview.jsx
 import React from 'react';
-import { Paper, Typography } from '@mui/material';
+import { Paper, Typography, Grid } from '@mui/material';
 
-function Overview() {
+function Overview({ weatherData }) {
+  // Function to format the timestamp into a readable time
+  const formatTime = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true, // or false if you want 24-hour time
+    });
+  };
+
   return (
     <Paper elevation={3} sx={{ height: '60%', padding: 2 }}>
-      {/* Content for the right top card */}
       <Typography variant='h6' component='h2'>
-        Overview
+        {weatherData?.name} Overview
       </Typography>
-      {/* Other details here */}
+      {weatherData && (
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant='body1'>
+              Weather Description: {weatherData.weather[0].main} ({weatherData.weather[0].description})
+            </Typography>
+            <Typography variant='body1'>Feels Like: {weatherData.main.feels_like}°C</Typography>
+            <Typography variant='body1'>Cloudiness: {weatherData.clouds.all}%</Typography>
+            <Typography variant='body1'>Pressure: {weatherData.main.pressure} hPa</Typography>
+            <Typography variant='body1'>Humidity: {weatherData.main.humidity}%</Typography>
+            <Typography variant='body1'>Visibility: {weatherData.visibility / 1000} km</Typography>
+            <Typography variant='body1'>Wind Speed: {weatherData.wind.speed} m/s</Typography>
+            <Typography variant='body1'>Wind Direction: {weatherData.wind.deg}°</Typography>
+            <Typography variant='body1'>Sunrise: {formatTime(weatherData.sys.sunrise)}</Typography>
+            <Typography variant='body1'>Sunset: {formatTime(weatherData.sys.sunset)}</Typography>
+          </Grid>
+        </Grid>
+      )}
     </Paper>
   );
 }
