1e8be9afd12c4830ac923ec723a07842dd8eade5
Léon Kiefer <leon.kiefer@proton.me>
2023-11-22 12:56:44 +0100

    Update API endpoint

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -28,7 +28,7 @@ function Dashboard({ onToggleTheme }) {
 
         // Fetch forecast data
         return axios.get(
-          `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
+          `https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&appid=${apiKey}&units=metric`
         );
       })
       .then((response) => {
