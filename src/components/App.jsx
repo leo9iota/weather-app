ceb8224c773520197972842ddb72eb1ed6381799
Léon Kiefer <leon.kiefer@proton.me>
2023-11-22 15:54:47 +0100

    Add theme provider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-// App.js
 import React from 'react';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import { ThemeProviderWrapper } from './ThemeContext'; // adjust the import path
 import Dashboard from './Dashboard';
 
 function App() {
   return (
     <ThemeProviderWrapper>
-      <ThemeProvider theme={theme.muiTheme}>
-        <CssBaseline />
-        <Dashboard />
-      </ThemeProvider>
+      <CssBaseline />
+      <Dashboard />
     </ThemeProviderWrapper>
   );
 }
