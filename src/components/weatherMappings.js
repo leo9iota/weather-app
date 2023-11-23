1e2a1851553376dcb9dad323a5e85e93ffb53cde
Léon Kiefer <leon.kiefer@proton.me>
2023-11-23 15:25:25 +0100

    Add weather image mappings

---
+++ src/components/weatherMappings.js
@@ -0,0 +1,46 @@
+// weatherMapping.js
+export const backgroundImages = {
+  '01d': 'clear-day',
+  '01n': 'clear-night',
+  '02d': 'few-clouds-day',
+  '02n': 'few-clouds-night',
+  '03d': 'cloudy-day', // Assuming 'scattered clouds' and 'cloudy' use the same image
+  '03n': 'cloudy-night',
+  '04d': 'cloudy-day', // Assuming 'broken clouds' and 'cloudy' use the same image
+  '04n': 'cloudy-night',
+  '09d': 'rain-day', // Assuming 'shower rain' uses the 'rain' image
+  '09n': 'rain-night',
+  '10d': 'rain-day',
+  '10n': 'rain-night',
+  '11d': 'storm-day', // Assuming 'thunderstorm' uses the 'storm' image
+  '11n': 'storm-night',
+  '13d': 'snow-day',
+  '13n': 'snow-night',
+  '50d': 'cloudy-day', // Assuming 'mist' uses the 'cloudy' image
+  '50n': 'cloudy-night',
+  // Default image if no match is found
+  default: 'default',
+};
+
+export const weatherSymbols = {
+  '01d': 'clear-day',
+  '01n': 'clear-night',
+  '02d': 'few-clouds-day',
+  '02n': 'few-clouds-night',
+  '03d': 'cloudy-day',
+  '03n': 'cloudy-night',
+  '04d': 'cloudy-day',
+  '04n': 'cloudy-night',
+  '09d': 'rain-day',
+  '09n': 'rain-night',
+  '10d': 'rain-day',
+  '10n': 'rain-night',
+  '11d': 'storm-day',
+  '11n': 'storm-night',
+  '13d': 'snow-day',
+  '13n': 'snow-night',
+  '50d': 'cloudy-day',
+  '50n': 'cloudy-night',
+  // Default symbol if no match is found
+  default: 'default',
+};
