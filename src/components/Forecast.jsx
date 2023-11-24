2bec5cc7a338f04f4d069e619d7a5088a715e3da
Léon Kiefer <leon.kiefer@proton.me>
2023-11-24 13:12:00 +0100

    Add weather icon to carousel

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,12 +1,28 @@
-import React, { useRef } from 'react';
-import { Paper, Typography, Grid, IconButton } from '@mui/material';
-import BackArrow from '@mui/icons-material/ArrowBackIosNewRounded';
-import ForwardArrow from '@mui/icons-material/ArrowForwardIosRounded';
+import React, { useRef, useState, useEffect } from 'react';
+import { Paper, Typography, IconButton, Box } from '@mui/material';
+import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { weatherSymbols } from './weatherMappings'; // Import your mappings
+import { useThemeContext } from './ThemeContext'; // Import the ThemeContext hook
 
 function Forecast({ forecastData, weatherData }) {
   const swiperRef = useRef(null);
+  const [weatherIcons, setWeatherIcons] = useState([]);
+  const { currentTheme } = useThemeContext(); // Use the theme context
+  const isDarkTheme = currentTheme === 'dark'; // Determine if the current theme is dark
+
+  useEffect(() => {
+    if (forecastData && forecastData.list) {
+      const icons = forecastData.list.map((item) => {
+        const iconCode = item.weather[0].icon;
+        const symbol = weatherSymbols[iconCode] || weatherSymbols.default;
+        return require(`../images/symbols/${symbol}.svg`); // Adjust this path to your SVG files
+      });
+      setWeatherIcons(icons);
+    }
+  }, [forecastData]);
 
   const goPrev = () => {
     if (swiperRef.current && swiperRef.current.swiper) {
@@ -18,6 +34,16 @@ function Forecast({ forecastData, weatherData }) {
     if (swiperRef.current && swiperRef.current.swiper) {
       swiperRef.current.swiper.slideNext();
     }
+  };
+
+  const frostedGlassStyle = {
+    backdropFilter: 'blur(10px)',
+    backgroundColor: isDarkTheme ? 'rgba(22, 22, 31, 0.6)' : 'rgba(190, 190, 190, 0.6)', // Adjust for dark theme if needed
+    borderRadius: '15px',
+    padding: '10px',
+    margin: '5px',
+    boxShadow: 'none',
+    color: isDarkTheme ? 'white' : 'black', // Adjust for dark theme if needed
   };
 
   return (
@@ -36,7 +62,7 @@ function Forecast({ forecastData, weatherData }) {
         }}
         onClick={goPrev}
       >
-        <BackArrow />
+        <ArrowBackIosNewRoundedIcon />
       </IconButton>
 
       <Swiper
@@ -49,18 +75,31 @@ function Forecast({ forecastData, weatherData }) {
         {forecastData && forecastData.list ? (
           forecastData.list.slice(0, 24).map((item, index) => (
             <SwiperSlide key={index}>
-              <Grid container spacing={2} sx={{ padding: 2 }}>
-                <Grid item xs={12}>
-                  <Typography variant='body1' sx={{ fontWeight: '', userSelect: 'none' }}>
-                    {new Date(item.dt * 1000).toLocaleTimeString()}
+              <Box sx={frostedGlassStyle}>
+                <Box sx={{ textAlign: 'center' }}>
+                  <Typography variant='body1' sx={{ fontWeight: 'bold', userSelect: 'none' }}>
+                    {new Date(item.dt * 1000).toLocaleTimeString([], {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                    })}
                   </Typography>
-                </Grid>
-                <Grid item xs={12}>
+                  <Box sx={{ width: '100px', height: '100px', margin: 'auto' }}>
+                    {' '}
+                    {/* Image container with fixed size */}
+                    <img
+                      src={weatherIcons[index]}
+                      alt={item.weather[0].description}
+                      style={{ maxWidth: '100%', maxHeight: '100%' }}
+                    />
+                  </Box>
                   <Typography variant='body2' sx={{ userSelect: 'none' }}>
-                    {item.weather[0].description}, Temp: {item.main.temp}°C
+                    {item.weather[0].description}
                   </Typography>
-                </Grid>
-              </Grid>
+                  <Typography variant='body2' sx={{ userSelect: 'none' }}>
+                    {`${item.main.temp_min.toFixed(1)}°C / ${item.main.temp_max.toFixed(1)}°C`}
+                  </Typography>
+                </Box>
+              </Box>
             </SwiperSlide>
           ))
         ) : (
@@ -78,7 +117,7 @@ function Forecast({ forecastData, weatherData }) {
         }}
         onClick={goNext}
       >
-        <ForwardArrow />
+        <ArrowForwardIosRoundedIcon />
       </IconButton>
     </Paper>
   );
