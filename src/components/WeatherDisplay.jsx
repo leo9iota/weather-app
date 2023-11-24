8658227320d4a7492c2825d094cf6d71c7a6ac1a
Léon Kiefer <leon.kiefer@proton.me>
2023-11-24 15:09:06 +0100

    Update weather display styling

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Typography, useTheme } from '@mui/material';
+import { Box, Paper, Typography, useTheme } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import NightsStayIcon from '@mui/icons-material/NightsStay';
@@ -69,92 +69,93 @@ function WeatherDisplay({ weatherData }) {
   const weatherSymbol = require(`../images/symbols/${icon}.svg`);
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        minHeight: '319px',
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        p: 0,
-        borderRadius: 2,
-        overflow: 'hidden',
-        boxSizing: 'border-box',
-        color: 'white',
-        textShadow: '1px 1px 2px black',
-        position: 'relative',
-      }}
-    >
-      {/* Top section for location and time */}
-      <Box
-        sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', p: 2 }}
-      >
-        {' '}
-        {/* Padding added here */}
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 0.5 }}>
-          <Typography variant='h6' sx={{ display: 'flex', alignItems: 'center' }}>
-            <LocationOnIcon />
-            {weatherData.name}
-          </Typography>
-          <Typography variant='subtitle2'>{weekday}</Typography>
-          <Typography variant='subtitle2'>{dateStr}</Typography>
-        </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-          <AccessTimeIcon />
-          <Typography variant='h6'>{time}</Typography>
-        </Box>
-      </Box>
-
+    <Paper elevation={3} sx={{ overflow: 'hidden' }}>
       <Box
         sx={{
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          right: 0,
-          pt: 0,
-          pb: 2,
-          pl: 2,
-          pr: 2,
           display: 'flex',
+          flexDirection: 'column',
           justifyContent: 'space-between',
-          alignItems: 'center',
+          minHeight: '319px',
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          p: 0,
+          boxSizing: 'border-box',
+          color: 'white',
+          textShadow: '1px 1px 2px black',
+          position: 'relative',
         }}
       >
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: 1.5 }}>
-          <Typography variant='h1' component='div' sx={{ fontSize: '3rem' }}>
-            {weatherData.main.temp.toFixed(1)}°C
-          </Typography>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            {dayTimeIcon}
-            <Typography variant='subtitle1' sx={{ ml: 1, fontSize: '1.25rem' }}>
-              {dayTimeText}
+        {/* Top section for location and time */}
+        <Box
+          sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', p: 2 }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 0.5 }}>
+            <Typography variant='h6' sx={{ display: 'flex', alignItems: 'center' }}>
+              <LocationOnIcon />
+              {weatherData.name}
             </Typography>
-            <FiberManualRecordIcon fontSize='small' sx={{ fontSize: '0.5rem', mx: 1 }} />
-            <Typography variant='body1' sx={{ fontSize: '1.25rem' }}>
-              {weatherData.main.temp_min.toFixed(1)}°C / {weatherData.main.temp_max.toFixed(1)}°C
-            </Typography>
+            <Typography variant='subtitle2'>{weekday}</Typography>
+            <Typography variant='subtitle2'>{dateStr}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <AccessTimeIcon />
+            <Typography variant='h6'>{time}</Typography>
           </Box>
         </Box>
+
+        {/* Bottom section for temperature and weather icon */}
         <Box
           sx={{
             position: 'absolute',
             bottom: 0,
-            right: -15,
-            height: '250px',
-            width: 'auto',
-            transform: 'translate(0, 0)',
-            userSelect: 'none',
-            pointerEvents: 'none',
+            left: 0,
+            right: 0,
+            pt: 0,
+            pb: 2,
+            pl: 2,
+            pr: 2,
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
           }}
-          component='img'
-          src={weatherSymbol}
-          draggable='false'
-        />
+        >
+          <Box
+            sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: 1.5 }}
+          >
+            <Typography variant='h1' component='div' sx={{ fontSize: '3rem' }}>
+              {weatherData.main.temp.toFixed(1)}°C
+            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              {dayTimeIcon}
+              <Typography variant='subtitle1' sx={{ ml: 1, fontSize: '1.25rem' }}>
+                {dayTimeText}
+              </Typography>
+              <FiberManualRecordIcon fontSize='small' sx={{ fontSize: '0.5rem', mx: 1 }} />
+              <Typography variant='body1' sx={{ fontSize: '1.25rem' }}>
+                {weatherData.main.temp_min.toFixed(1)}°C / {weatherData.main.temp_max.toFixed(1)}°C
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              bottom: 0,
+              right: -15,
+              height: '250px',
+              width: 'auto',
+              transform: 'translate(0, 0)',
+              userSelect: 'none',
+              pointerEvents: 'none',
+            }}
+            component='img'
+            src={weatherSymbol}
+            draggable='false'
+          />
+        </Box>
       </Box>
-    </Box>
+    </Paper>
   );
 }
 
