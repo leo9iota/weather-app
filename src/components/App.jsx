c0dbb6053accaf5da7524a1b0292507389ca2306
Léon Kiefer <leon.kiefer@proton.me>
2024-01-10 09:09:02 +0100

    Remove title anim
    Remove `gamesense.pub` inspired title animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { ThemeProviderWrapper } from './ThemeContext';
 import Dashboard from './Dashboard';
-// import TitleAnim from './TitleAnim';
 
 function App() {
   return (
     <ThemeProviderWrapper>
       <CssBaseline />
-      {/* <TitleAnim /> */}
       <Dashboard />
     </ThemeProviderWrapper>
   );
