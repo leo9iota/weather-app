7f25fb000270f44531445cbd8c2714248bc5553e
Léon Kiefer <leon.kiefer@proton.me>
2024-01-11 11:12:00 +0100

    Refactor dynamic import
    Refactor dynamic import and remove commonJS import

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -3,55 +3,73 @@ import { Paper, Typography, IconButton, Box } from '@mui/material';
 import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { weatherSymbols } from './weatherMappings';
+import { useThemeContext } from './ThemeContext';
 import 'swiper/css';
-import { weatherSymbols } from './weatherMappings'; // Import your mappings
-import { useThemeContext } from './ThemeContext'; // Import the ThemeContext hook
 
-const Forecast = ({ forecastData, weatherData }) => {
+function Forecast({ forecastData, weatherData }) {
+  // Reference for the swiper component to control its behavior programmatically
   const swiperRef = useRef(null);
-  const [weatherIcons, setWeatherIcons] = useState([]);
-  const { currentTheme } = useThemeContext(); // Use the theme context
-  const isDarkTheme = currentTheme === 'dark'; // Determine if the current theme is dark
 
+  // State to store weather icons
+  const [weatherIcons, setWeatherIcons] = useState([]);
+
+  // Accessing the current theme from context
+  const { currentTheme } = useThemeContext();
+  const isDarkTheme = currentTheme === 'dark'; // Checking if the theme is dark
+
+  // Effect to update weather icons whenever forecast data changes
   useEffect(() => {
     if (forecastData && forecastData.list) {
-      const icons = forecastData.list.map((item) => {
-        const iconCode = item.weather[0].icon;
-        const symbol = weatherSymbols[iconCode] || weatherSymbols.default;
-        return require(`../images/symbols/${symbol}.svg`); // Adjust this path to your SVG files
-      });
-      setWeatherIcons(icons);
+      const loadIcons = async () => {
+        const icons = await Promise.all(
+          forecastData.list.map(async (item) => {
+            const iconCode = item.weather[0].icon;
+            const symbol = weatherSymbols[iconCode] || weatherSymbols.default;
+            const iconModule = await import(`../images/symbols/${symbol}.svg`);
+            return iconModule.default; // Use .default to get the actual module value
+          })
+        );
+        setWeatherIcons(icons);
+      };
+
+      loadIcons();
     }
   }, [forecastData]);
 
+  // Function to navigate to the previous slide
   const goPrev = () => {
     if (swiperRef.current && swiperRef.current.swiper) {
       swiperRef.current.swiper.slidePrev();
     }
   };
 
+  // Function to navigate to the next slide
   const goNext = () => {
     if (swiperRef.current && swiperRef.current.swiper) {
       swiperRef.current.swiper.slideNext();
     }
   };
 
+  // Styling for the frosted glass effect, adjusted based on the theme
   const frostedGlassStyle = {
     backdropFilter: 'blur(10px)',
-    backgroundColor: isDarkTheme ? 'rgba(22, 22, 31, 0.6)' : 'rgba(190, 190, 190, 0.6)', // Adjust for dark theme if needed
+    backgroundColor: isDarkTheme ? 'rgba(22, 22, 31, 0.6)' : 'rgba(190, 190, 190, 0.6)',
     borderRadius: '15px',
     padding: '10px',
     margin: '5px',
     boxShadow: 'none',
-    color: isDarkTheme ? 'white' : 'black', // Adjust for dark theme if needed
+    color: isDarkTheme ? 'white' : 'black',
   };
 
   return (
     <Paper elevation={3} sx={{ padding: 2, position: 'relative', paddingLeft: 4, paddingRight: 4 }}>
+      {/* Displaying the location's name */}
       <Typography variant='h2' component='h2' sx={{ marginBottom: 2 }}>
         {weatherData?.name} Forecast
       </Typography>
 
+      {/* Navigation buttons */}
       <IconButton
         sx={{
           position: 'absolute',
@@ -65,6 +83,7 @@ const Forecast = ({ forecastData, weatherData }) => {
         <ArrowBackIosNewRoundedIcon />
       </IconButton>
 
+      {/* Swiper carousel for showing the forecast */}
       <Swiper
         ref={swiperRef}
         spaceBetween={50}
@@ -76,6 +95,7 @@ const Forecast = ({ forecastData, weatherData }) => {
           forecastData.list.slice(0, 24).map((item, index) => (
             <SwiperSlide key={index}>
               <Box sx={frostedGlassStyle}>
+                {/* Displaying weather details for each forecast item */}
                 <Box sx={{ textAlign: 'center' }}>
                   <Typography variant='body1' sx={{ fontWeight: 'bold', userSelect: 'none' }}>
                     {new Date(item.dt * 1000).toLocaleTimeString([], {
@@ -84,8 +104,6 @@ const Forecast = ({ forecastData, weatherData }) => {
                     })}
                   </Typography>
                   <Box sx={{ width: '100px', height: '100px', margin: 'auto' }}>
-                    {' '}
-                    {/* Image container with fixed size */}
                     <img
                       src={weatherIcons[index]}
                       alt={item.weather[0].description}
